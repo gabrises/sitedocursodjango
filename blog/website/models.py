ff095da34a2a82b55b7d627a5ff88278ea0e5b69
gabrises <gvap120@hotmail.com>
2020-09-30 22:44:38 -0300

    some modifications admin section

--- blog/website/models.py
+++ blog/website/models.py
@@ -4,3 +4,11 @@ class Post(models.Model):
  	title = models.CharField(max_length=100)
  	sub_title = models.CharField(max_length=300)
  	content = models.TextField()
+
+ 	def __str__(self):
+ 		return self.title
+
+ 	def full_name(self):
+ 		return self.title + " - " + self.sub_title
+
+ 	full_name.admin_order_field = 'title'
