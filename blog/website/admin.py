19c86d159c2aefd3f9a2e90f452afc760f0823c6
gabrises <gvap120@hotmail.com>
2020-10-01 22:48:44 -0300

    queryset

--- blog/website/admin.py
+++ blog/website/admin.py
@@ -3,8 +3,14 @@ from .models import Post
 
 
 class PostAdmin(admin.ModelAdmin):
-	list_display = ['title', 'sub_title', 'full_name', 'categories']
+	list_display = [
+		'title', 'sub_title', 'full_name',
+		'categories', 'deleted', 
+		]
 	search_fields = ['title', 'sub_title']
 	#fields = ('title', 'sub_title', 'content')
+
+	def get_queryset(self, request):
+		return Post.objects.filter(deleted=False)
 
 admin.site.register(Post, PostAdmin)
