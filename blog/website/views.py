935dfbd9d691ae3d2514ce6dba0b5b62469f4822
gabrises <gvap120@hotmail.com>
2020-10-01 23:44:50 -0300

    readin db posts and showin at html5 strata

--- blog/website/views.py
+++ blog/website/views.py
@@ -3,7 +3,7 @@ from .models import Post
 
 def hello_blog(request):
 	lista = ['django', 'python', 'html', 'gabriel']
-	list_posts = Post.objects.all()
+	list_posts = Post.objects.filter(deleted=False)
 	data = {
 		'name': 'Curso de Django 3', 
 		'listatec': lista, 
